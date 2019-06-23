a792db5dd3a21bb18e22d3d3b3f82ffe66538299
Zach Dyer <zach.dyer@gmail.com>
2019-06-23 17:50:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,23 +54,24 @@ function setupCanvas(canvas) {
 
 var json = []
 
-var starsystem = new StarSystem(0)
-var player = new Player(0, starsystem.address)
-var spaceship = new SpaceShip(0, starsystem.address)
-var loader = document.getElementById("loader")
-var jumbotron = document.getElementById("jumbotron")
-var canvas = setupCanvas(jumbotron)
-var jumbotronTitle = document.getElementById("jumbo-title")
-var jumboText = document.getElementById("jumbo-text")
-var scanner = document.getElementById("scanner")
-var cards = document.getElementById("cards")
-var planets = []
-var planetButtons = []
+var starsystem =      new StarSystem(0)
+var player =          new Player(0, starsystem.address)
+var spaceship =       new SpaceShip(0, starsystem.address)
+var loader =          document.getElementById("loader")
+var jumbotron =       document.getElementById("jumbotron")
+// var canvas =          setupCanvas(jumbotron)
+var jumbotronTitle =  document.getElementById("jumbo-title")
+var jumboText =       document.getElementById("jumbo-text")
+var scanner =         document.getElementById("scanner")
+var cards =           document.getElementById("cards")
+var fuelLevel =       document.getElementById("fuel-level")
+var planets =         []
+var planetButtons =   []
 scanner.addEventListener("click", function(){
   console.log("Scanning...")
-  if(starsystem.planetTotal && starsystem.scanned == false) {
+  if(starsystem.planetTotal && starsystem.scanned === false) {
     starsystem.scanned = true
-    jumboText.innerHTML = "You discovered " + starsystem.planetTotal + " planets in this system."
+    jumboText.innerHTML = starsystem.jumbotext() + " You discovered " + starsystem.planetTotal + " planets in this system."
     cards.innerHTML = ""
     for(var i=0;i<starsystem.planetTotal;i++) {
       let planet = new Planet(starsystem.id, i)
@@ -78,11 +79,68 @@ scanner.addEventListener("click", function(){
       let card = "<div class=\"col-lg-3 col-md-6 mb-4\"><div class=\"card h-100\"><img class=\"card-img-top\" src=\"img/planet.jpg\" alt=\"Card image cap\"><div class=\"card-body\"><h4 class=\"card-title\">Planet "+planet.name+"</h4><p class=\"card-text\">The planet "+planet.name+" has "+planet.moons+" moons and has "+planet.size+" areas to explore and is "+planet.starDistance+" distance from it's host star.</p></div><div class=\"card-footer\"><button class=\"btn btn-primary\" id=\"planet"+i+"\" data-planet-id=\""+planet.id+"\">Orbit</button></div></div></div>"
       cards.innerHTML += card
     }
+    //Planets
     for(var i = 0; i < starsystem.planetTotal; i++) {
       planetButtons.push(document.getElementById("planet"+i))
+      //Orbit planet
       planetButtons[i].addEventListener("click", function(event){
+        starsystem.scanned = false
         let planetID = event.target.getAttribute("data-planet-id")
         let planet = planets[planetID]
+        let travelDistance = Math.abs(planet.starDistance - player.starDistance)
+        if(spaceship.fuel >= travelDistance) {
+          spaceship.fuel -= travelDistance
+          let fuelPercent = Math.trunc((spaceship.fuel / spaceship.fuelMax) * 100)
+          fuelLevel.style.width = fuelPercent + "%"
+          player.starDistance = planet.starDistance
+          player.address = planet.address
+          spaceship.address = planet.address
+          // let planetImg = new Image()
+          // planetImg.src = "img/planet.jpg"
+          jumbotron.src = "img/planet.jpg"
+          //clear canvas
+          // canvas.clearRect(0,0,jumbotron.width,jumbotron.height)
+          //draw planet image on canvas
+          // canvas.drawImage(planetImg,0,0)
+          jumbotronTitle.innerHTML = planet.name
+          jumboText.innerHTML = "You traveled "+travelDistance+" distance and are orbiting the planet " + planet.name + " with " + planet.moons + " moons. This planet orbits " + planet.starDistance + " distance from it's host star."
+          console.log(planet)
+          window.scrollTo(0,0)
+        } else {
+          console.error("Not enough fuel")
+        }
+      })
+    }
+    //Star Card
+    let col = document.createElement("div")
+    col.className = "col-lg-3 col-md-6 mb-4"
+    let image = new Image()
+    image.src = "img/sun.jpg"
+    image.className = "card-img-top"
+    let h4 = document.createElement("h4")
+    h4.innerHTML = starsystem.name + " Star"
+    let p = document.createElement("p")
+    p.className = "card-text"
+    p.innerHTML = "The " + starsystem.name + " star system."
+    let footer = document.createElement("div")
+    footer.className = "card-footer"
+    let orbitButton = document.createElement("button")
+    orbitButton.innerHTML = "Orbit"
+    orbitButton.className = "btn btn-primary"
+    orbitButton.setAttribute("data-star-id", starsystem.id)
+    orbitButton.id = "star" + starsystem.id
+    orbitButton.addEventListener("click", function(){
+     if(spaceship.fuel >= travelDistance) {
+        starsystem.scanned = false
+        let planetID = event.target.getAttribute("data-planet-id")
+        let planet = planets[planetID]
+        let travelDistance = Math.abs(planet.starDistance - player.starDistance)
+        spaceship.fuel -= travelDistance
+        let fuelPercent = Math.trunc((spaceship.fuel / spaceship.fuelMax) * 100)
+        fuelLevel.style.width = fuelPercent + "%"
+        player.starDistance = planet.starDistance
+        player.address = planet.address
+        spaceship.address = planet.address
         let planetImg = new Image()
         planetImg.src = "img/planet.jpg"
         //clear canvas
@@ -90,11 +148,22 @@ scanner.addEventListener("click", function(){
         //draw planet image on canvas
         canvas.drawImage(planetImg,0,0)
         jumbotronTitle.innerHTML = planet.name
-        jumboText.innerHTML = "You are orbiting the planet " + planet.name + " with " + planet.moons + " moons. This planet orbits " + planet.starDistance + " distance from it's host star."
+        jumboText.innerHTML = "You traveled "+travelDistance+" distance and are orbiting the planet " + planet.name + " with " + planet.moons + " moons. This planet orbits " + planet.starDistance + " distance from it's host star."
         console.log(planet)
         window.scrollTo(0,0)
-      })
-    }
+      } else {
+        console.error("Not enough fuel")
+      }
+    })
+    footer.appendChild(orbitButton)
+    col.appendChild(image)
+    col.appendChild(h4)
+    col.appendChild(p)
+    col.appendChild(footer)
+    
+    // let card = "<div class=\"col-lg-3 col-md-6 mb-4\"><div class=\"card h-100\"><img class=\"card-img-top\" src=\"img/planet.jpg\" alt=\"Card image cap\"><div class=\"card-body\"><h4 class=\"card-title\">Star "+starsystem.name+"</h4><p class=\"card-text\">The "+starsystem.name+" star system.</p></div><div class=\"card-footer\"><button class=\"btn btn-primary\" id=\"star"+starsystem.id+"\" data-star-id=\""+starsystem.id+"\">Orbit</button></div></div></div>"
+    cards.appendChild(col)
+    
   }
   
 })
@@ -103,7 +172,8 @@ scanner.addEventListener("click", function(){
 jumbotron.style.backgroundColor = "black"
 
 //Draw stars
-starsystem.draw(canvas)
+// starsystem.draw(canvas)
+jumbotron.src = "img/sun.jpg"
 
 //Update jumbotron title
 jumbotronTitle.innerHTML = starsystem.name
